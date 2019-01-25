dd2d09444940113e118d5ed57f3c32e31beb22f5
macjabeth <macbethjonathan@gmail.com>
2019-01-25 18:02:03 +0000

    convert dinosaurs to objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,28 +6,54 @@
   object name, diet, weight, length, period
 */
 
+// function Dinosaur (attributes) {
+//   this.name = attributes.name;
+//   this.diet = attributes.diet;
+//   this.weight = attributes.weight;
+//   this.length = attributes.length;
+//   this.period = attributes.period;
+// }
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
-const tyrannosaurus = ['carnivorous', '7000kg', '12m', 'Late Cretaceious'];
+const tyrannosaurus = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceious'
+};
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-const stegosaurus = ['herbivorous', '2000kg', '9m', 'Late Jurassic'];
+const stegosaurus = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+};
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
-const velociraptor = ['carnivorous', '15kg', '1.8m', 'Late Cretaceious'];
+const velociraptor = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceious'
+};
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log(tyrannosaurus[1]);
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log(velociraptor[0]);
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log(stegosaurus[2]);
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log(tyrannosaurus[3]);
+console.log(tyrannosaurus.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
 tyrannosaurus.roar = () => 'RAWERSRARARWERSARARARRRR!';
@@ -68,7 +94,7 @@ Name email@example.com
 Log the result of your new array. */
 const contactInfo = [];
 for (let i = 0; i < graduates.length; i++) {
-  const graduate = graduates[i]
+  const graduate = graduates[i];
   contactInfo.push(`${graduate.first_name} ${graduate.email}`);
 }
 console.log(contactInfo);
