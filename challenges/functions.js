1e51ad771cdf3aa4fc37a68fed41c608e8a1ddf5
macjabeth <macbethjonathan@gmail.com>
2019-01-25 18:14:59 +0000

    convert functions to es6 syntax

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,24 +6,16 @@
   * The last parameter accepts a callback
   * In the body of the function return the callback with the two parameters that you created
 */
-function consume (first, second, callback) {
-  return callback(first, second);
-}
+const consume = (first, second, callback) => callback(first, second);
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-function add (a, b) {
-  return a + b;
-}
-function multiply (a, b) {
-  return a * b;
-}
-function greeting (first, last) {
-  return `Hello ${first} ${last}, nice to meet you!`;
-}
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const greeting = (first, last) => `Hello ${first} ${last}, nice to meet you!`;
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 console.log(consume(2, 2, add)); // 4
